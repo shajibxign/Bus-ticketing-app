7493df8eff30fd329beda5eae6466e65ee371a9c
SHAHREAR AHMED SHAJIB <66648834+shajib1927@users.noreply.github.com>
2022-12-31 01:48:34 +0300

    Update booking.php

--- admin/booking.php
+++ admin/booking.php
@@ -1,4 +1,4 @@
-<!-- Show these admin pages only when the admin is logged in -->
+<!-- Show these admin pages only when the admin is logged-in -->
 <?php  require '../assets/partials/_admin-check.php';   ?>
 
 
